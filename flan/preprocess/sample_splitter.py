ffa8170bcc47d64cc5ec75a4241cf255dff6876b
TheVidz <yovidit2004@gmail.com>
2026-05-24 20:37:35 +0530

    coment-out commit e9e9d8a

--- flan/preprocess/sample_splitter.py
+++ flan/preprocess/sample_splitter.py
@@ -30,8 +30,8 @@ class FoldSplitter:
             random_state (int): Fixed random_state for train_test_split sklearn function
         """
         # adding min 5 folds
-        num_folds = getattr(self.args, "num_folds", 5)
-        self.args.num_folds = num_folds
+        # num_folds = getattr(self.args, "num_folds", 5)
+        # self.args.num_folds = num_folds
 
         ids = pandas.read_table(cache.ids_path()).rename(columns={'#IID': 'IID'}).filter(['FID', 'IID'])
         indices = numpy.arange(ids.shape[0])
